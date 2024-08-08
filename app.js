fc7082f9bfa3d1a584259bcca2d92351e461aafc
chuyenpn <chuyenpn@rikkeisoft.com>
2024-08-08 17:03:31 +0700

    Update

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const axios = require("axios");
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+let users = [{ name: "Alice" }, { name: "Bob" }];
+app.get("/users", (req, res) => {
+  res.json(users);
+});
